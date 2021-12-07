b4b3bd6308853f2f92db4292bf7b98d2eff90bfe
Benjamin <Benjamin>
2021-12-07 23:08:38 +0000

    deleted maxpool

--- resnet.py
+++ resnet.py
@@ -196,7 +196,6 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(num_channels, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
